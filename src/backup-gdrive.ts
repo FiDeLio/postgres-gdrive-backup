73f86cb622e698e3af2f1b273c0d453a9dcbb113
FiDeLio <fidel.oyarzo@gmail.com>
2024-09-12 19:58:25 -0300

    custom

--- src/backup-gdrive.ts
+++ src/backup-gdrive.ts
@@ -18,47 +18,40 @@ const auth = new JWT({
 
 const gdrive = drive({
   version: "v3",
-  auth: auth
+  auth: auth,
 });
 
 const deleteStaleBackups = async (cutOffDate: Date) => {
-  try {
-    const folderAccess = await gdrive.files.get({
-      fileId: env.FOLDER_ID,
-      fields: "id",
-    });
+  const folderAccess = await gdrive.files.get({
+    fileId: env.FOLDER_ID,
+    fields: "id",
+  });
 
-    if (!folderAccess.data.id) {
-      console.error(`No access to FOLDER_ID: ${env.FOLDER_ID}`);
-      return;
-    }
+  if (!folderAccess.data.id) {
+    console.error(`No access to FOLDER_ID: ${env.FOLDER_ID}`);
+    return;
+  }
 
-    const res = await gdrive.files.list({
-      pageSize: 100,
-      fields: "nextPageToken, files(id, createdTime)",
-      q: `'${env.FOLDER_ID}' in parents and trashed=false and mimeType = 'application/gzip' and createdTime < '${cutOffDate.toISOString()}'`,
-    });
+  const res = await gdrive.files.list({
+    pageSize: 100,
+    fields: "nextPageToken, files(id, createdTime)",
+    q: `'${env.FOLDER_ID}' in parents and trashed=false and mimeType = 'application/gzip' and createdTime < '${cutOffDate.toISOString()}'`,
+  });
 
-    if (!res.data.files || res.data.files.length === 0) {
-      console.log("No old backups found for deletion.");
-      return;
-    }
+  if (!res.data.files) {
+    return;
+  }
 
-    for (const file of res.data.files) {
-      if (file.id) {
-        await gdrive.files.delete({ fileId: file.id });
-        console.log(`Deleted backup file with ID: ${file.id}`);
-      }
-    }
-  } catch (error) {
-    console.error("Error deleting old backups:", error);
+  for (const file of res.data.files) {
+    if (!file.id) continue;
+    await gdrive.files.delete({ fileId: file.id });
   }
 };
 
-const dumpToFile = async (filepath: string) => {
+const dumpToFile = async (path: string) => {
   return new Promise((resolve, reject) => {
     exec(
-      `pg_dump --dbname=${env.DATABASE_URL} --format=custom | gzip > ${filepath}`,
+      `pg_dump --dbname=${env.DATABASE_URL} --format=custom | gzip > ${path}`,
       (err, stdout, stderr) => {
         if (err) {
           reject({
@@ -68,67 +61,65 @@ const dumpToFile = async (filepath: string) => {
           return;
         }
 
-        if (stderr) {
-          console.warn(stderr.trimEnd());
+        if (!!stderr) {
+          console.log(stderr.trimEnd());
         }
 
-        const isFileValid = execSync(`gzip -cd ${filepath} | head -c1`).length > 0;
+        const isFileValid = execSync(`gzip -cd ${path} | head -c1`).length > 0;
 
         if (!isFileValid) {
-          console.error("Backup file is empty or corrupted.");
-          reject("Backup file is empty or corrupted.");
+          console.error("Backup file is empty");
+          reject("Backup file is empty");
           return;
         }
 
-        console.log(`Backup file size: ${filesize(statSync(filepath).size)}`);
-        console.log(`Backup file created at: ${filepath}`);
+        console.log(`Backup file size: ${filesize(statSync(path).size)}`);
+        console.log(`Backup file created at: ${path}`);
+
+        if (stdout) {
+          console.log(stdout);
+        }
 
         resolve(stdout);
-      }
+      },
     );
   });
 };
 
-const pushToDrive = async (filename: string, filepath: string) => {
-  try {
-    const folderAccess = await gdrive.files.get({
-      fileId: env.FOLDER_ID,
-      fields: "id",
-    });
+const pushToDrive = async (filename: string, path: string) => {
+  const folderAccess = await gdrive.files.get({
+    fileId: env.FOLDER_ID,
+    fields: "id",
+  });
 
-    if (!folderAccess.data.id) {
-      console.error(`No access to FOLDER_ID: ${env.FOLDER_ID}`);
-      return;
-    }
-
-    const fileMetadata = {
-      name: filename,
-      parents: [env.FOLDER_ID],
-    };
-
-    const media = {
-      mimeType: "application/gzip",
-      body: createReadStream(filepath),
-    };
-
-    await gdrive.files.create({
-      requestBody: fileMetadata,
-      media: media,
-    });
-
-    console.log(`Backup ${filename} uploaded to Google Drive!`);
-  } catch (error) {
-    console.error("Error uploading backup to Google Drive:", error);
+  if (!folderAccess.data.id) {
+    console.error(`No access to FOLDER_ID: ${env.FOLDER_ID}`);
+    return;
   }
+
+  const fileMetadata = {
+    name: filename,
+    parents: [env.FOLDER_ID],
+  };
+
+  const media = {
+    mimeType: "application/gzip",
+    body: createReadStream(path),
+  };
+
+  await gdrive.files.create({
+    requestBody: fileMetadata,
+    media: media,
+  });
 };
 
 export async function run() {
   try {
     if (env.RETENTION && env.RETENTION !== "disabled") {
-      console.log(`Deleting old backups older than ${env.RETENTION} day(s)`);
-      const cutOffDate = dayjs().subtract(env.RETENTION, 'day').toDate();
+      console.log(`Deleting old backups older than a ${env.RETENTION}`);
+      const cutOffDate = dayjs().subtract(1, env.RETENTION).toDate();
       await deleteStaleBackups(cutOffDate);
-      console.log("Old backups deletion complete.");
+      console.log(`Delete complete! Procceding with backup.`);
     }
 
     const timestamp = new Date()
@@ -137,17 +128,23 @@ export async function run() {
       .replace(".", "-");
 
     const filename = `${env.FILE_PREFIX}${timestamp}.tar.gz`;
+
     const filepath = path.join(os.tmpdir(), filename);
 
-    console.log(`Starting backup: ${filename}`);
+    console.log(`Starting backup of ${filename}`);
+
     await dumpToFile(filepath);
 
-    console.log("Backup complete. Uploading to Google Drive...");
+    console.log("Backup done! Uploading to Google Drive...");
+
     await pushToDrive(filename, filepath);
 
+    console.log("Backup uploaded to Google Drive!");
+
     await unlink(filepath);
-    console.log("Backup file removed locally.");
-  } catch (error) {
-    console.error("Something went wrong during the backup process:", error);
+
+    console.log("All done!");
+  } catch (err) {
+    console.error("Something went wrong:", err);
   }
 }
